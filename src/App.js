1cc0c9e7ef96281acf3ba2aef9c6d2e8752d9f59
john681611 <john681611@hotmail.com>
2019-08-19 22:53:24 +0100

    Add Main Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+import Header from './components/Header';
+import News from './components/News';
+import Forum from './components/Forum';
+import About from './components/About';
+import Join from './components/Join';
+import Servers from './components/Servers';
+import Rules from './components/Rules';
+import Links from './components/Links';
+import CMDmsg from './components/CMDmsg';
+import Donate from './components/Donate';
+import Donators from './components/Donators';
+import Roster from './components/Roster';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav/>
+      <Header/>
+
+      <section id="news" class="section">
+          <div class="section__inner">
+              <News/>
+          </div>
+      </section>
+
+      <section id="forum" class="section">
+          <div class="section__inner">
+              <Forum/>
+          </div>
+      </section>
+
+      <section id="about" class="section">
+          <div class="section__inner">
+              <About/>
+          </div>
+      </section>
+
+      <section id="join" class="section">
+          <div class="section__inner">
+            <Join/>
+          </div>
+      </section>
+
+      <section id="servers" class="section">
+          <div class="section__inner">
+              <Servers/>
+          </div>
+      </section>
+
+      <section id="rules" class="section">
+          <div class="section__inner">
+            <Rules/>
+          </div>
+      </section>
+
+      <section id="links" class="section">
+          <div class="section__inner">
+            <Links/>
+          </div>
+      </section>
+
+      <section id="cmdmsg" class="section">
+          <div class="section__inner">
+            <CMDmsg/>
+          </div>
+      </section>
+
+      <section id="roster" class="section">
+          <div class="section__inner">
+            <Roster/>
+          </div>
+      </section>
+
+      <section id="donate" class="section">
+          <div class="section__inner">
+            <Donate/>
+          </div>
+      </section>
+
+      <section id="donators" class="section">
+          <div class="section__inner">
+              <Donators/>
+          </div>
+      </section>
+    
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Donators.js
@@ -0,0 +1,9 @@
+import React from 'react'
+ 
+const Header = (props) => {
+  return (
+    <div>Bla</div>
+  )
+}
+ 
+export default Header
