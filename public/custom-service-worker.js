ced31680de3dd1984cf9830f13a42050d7b788c0
john681611 <john681611@hotmail.com>
2020-03-20 13:49:34 +0000

    new notification setup

--- public/custom-service-worker.js
+++ public/custom-service-worker.js
@@ -1,51 +1,20 @@
 importScripts(`/service-worker.js`)
 
-self.addEventListener('push', function (e) {
-    var options = {
-        body: 'This notification was generated from a push!',
-        icon: 'images/logo-desktop.png',
-        vibrate: [100, 50, 100],
-        data: {
-            dateOfArrival: Date.now(),
-            primaryKey: '2'
-        },
-        actions: [
-            {
-                action: 'explore', title: 'Explore this new world',
-                icon: 'images/checkmark.png'
-            },
-            {
-                action: 'close', title: 'Close',
-                icon: 'images/xmark.png'
-            },
-        ]
+self.addEventListener('push', event => {
+    const title = 'Iron-Fists';
+    const data = JSON.parse(event.data.text());
+    const options = {
+        body: `${data.message}`,
+        icon: '/img/logo-desktop.png',
+        badge: '/img/logo-desktop.png',
+        data: data.url || 'http://ironfists.azurewebsites.net/#news'
     };
-    e.waitUntil(
-        self.registration.showNotification('Hello world!', options)
+    event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', event => {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow(event.notification.data)
     );
-});
-
-
-
-self.addEventListener('notificationclose', function (e) {
-    var notification = e.notification;
-    var data = notification.data || {};
-    var primaryKey = data.primaryKey;
-    console.debug('Closed notification: ' + primaryKey);
-});
-
-self.addEventListener('notificationclick', function (e) {
-    var notification = e.notification;
-    var data = notification.data || {};
-    var primaryKey = data.primaryKey;
-    var action = e.action;
-    console.debug('Clicked notification: ' + primaryKey);
-    if (action === 'close') {
-        console.debug('Notification clicked and closed', primaryKey);
-        notification.close();
-    }
-    else {
-        console.debug('Notification actioned', primaryKey);
-        notification.close();
-    }
 });
