1c28c72bd8097231a96c4d5264f1ec93e8aefcf0
john681611 <john681611@hotmail.com>
2026-01-06 14:40:10 +0000

    better logos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import reforgerLogo from '../images/arma-reforger-logo.png'
 
 const Header = (props) => {
   return (
     <section className="intro">
       <div>
+          <img alt="" src={reforgerLogo}/>
           <img alt="" className="lazyload" data-src="/images/logo-desktop.png"/>
-          <h1>Iron-Fists</h1>
           <b>Play Fair and with Honour</b>
     </div>
   </section>
